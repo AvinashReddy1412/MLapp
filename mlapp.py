ec5e52faa006b4c688e310fb6983a6c658e4ec1a
Avinash Reddy Rangu <avinash.reddy@berrijam.com>
2021-08-05 11:40:46 +0530

    Initial commit

---
+++ mlapp.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas as pd
+from data_analyzer import DataAnalyzer
+
+from PIL import Image
+
+st.title('Anthem AutoML')
+image = Image.open('Logo.png')
+st.image(image,use_column_width=True)
+
+def auto_ml():
+    activities = ['EDA', 'Review', 'model', 'About us']
+    option = st.sidebar.selectbox('Selection option:', activities)
+
+    if option == 'EDA':
+        st.subheader("Exploratory Data Analysis")
+
+        data = st.file_uploader("Upload dataset:", type=['csv', 'xlsx', 'txt', 'json'])
+        st.success("Data successfully loaded")
+
+        if data is not None:
+            df = pd.read_csv(data)
+            st.dataframe(df.head(10))
+
+            if st.checkbox("Display shape"):
+                st.write(df.shape)
+            if st.checkbox("Display columns"):
+                st.write(df.columns)
+            if st.checkbox("Review data"):
+                da = DataAnalyzer(dataframe=data)
+                st.write(da.get_analysis())
+            if st.checkbox('Display Correlation of data various columns'):
+                st.write(df.corr())
+
+    if option == "Review":
+        st.subheader("Review Uploaded Data")
+
